25ba7b639d2be89308ba9dca53bfbda3720081fb
AKFosterDev <akfoster2015@gmail.com>
2020-11-28 07:00:31 -0800

    begin js challenges

--- code-challenges.js
+++ code-challenges.js
@@ -2,14 +2,19 @@
 
 // --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
 
-var collections = ["purple", "blue", "green", "yellow", "pink"]
+var collections = ['purple', 'blue', 'green', 'yellow', 'pink']
 // Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
+// Create a function that takes an array as an argument
+// remove the first item from the array
+// create a conditional if array empty
+// return 'The array is empty'
+// otherwise shuffle the remaining content and return the array
 
-
-
+// call the function
+console.log('Problem #1')
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -17,10 +22,7 @@ var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
-
-
-
-
+console.log('Problem #2')
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
@@ -28,24 +30,18 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
-
-
-
-
+console.log('Problem #3')
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
 
-var testString1 = "albatross"
+var testString1 = 'albatross'
 // Expected output: "aLbAtRoSs"
-var testString2 = "jabberwocky"
+var testString2 = 'jabberwocky'
 // Expected output: "jAbBeRwOcKy"
-
-
-
-
-
+console.log('Problem #4')
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+console.log('Problem #5')
